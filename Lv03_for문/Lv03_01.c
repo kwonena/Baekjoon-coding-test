f998ef6132ee5498600503daddc6503ab93effa1
KwonEna <eeena214@gmail.com>
2021-08-03 23:51:48 +0900

    Add : Lv03_for문_01

---
+++ Lv03_for문/Lv03_01.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+
+    num = 2;
+    //scanf("%d", &num);
+
+    for(int i = 1; i <= 9; i++) {
+        printf("%d * %d = %d\n", num, i, num * i);
+    }
+}
---
+++ Lv03_for문/Lv03_01.js
@@ -0,0 +1,14 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  
+  rl.question('Number : ', (num) => {
+    for(let i = 1; i <= 9; i++) {
+        console.log(`${num} * ${i} = ${num * i}`);
+    }
+    rl.close();
+  });
+
